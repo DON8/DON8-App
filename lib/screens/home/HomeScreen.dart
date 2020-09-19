0de9cb377b292c66a45291f486e5ed802ce9db8a
kadircekim07 <33603691+kadircekim07@users.noreply.github.com>
2020-09-19 17:22:10 +0300

    Better home

--- lib/screens/home/HomeScreen.dart
+++ lib/screens/home/HomeScreen.dart
@@ -22,28 +22,150 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(240, 240, 240, 1),
-      appBar: PreferredSize(
-        preferredSize: new Size.fromHeight(72),
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 12),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(48),
+      body: ListView(
+        children: [
+          Transform.translate(
+            offset: Offset(0, -30),
+            child: PreferredSize(
+              preferredSize: new Size.fromHeight(72),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 12),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(48),
+                  ),
+                ),
+                child: Container(
+                  margin: EdgeInsets.only(top: 12),
+                  child: Image.asset("assets/logo.png", height: 72),
+                ),
+              ),
             ),
           ),
-          child: Container(
-            margin: EdgeInsets.only(top: 12),
-            child: Image.asset("assets/logo.png", height: 72),
+
+          Center(child: CustomCarouselSlider(items: sliderItems)),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  child: FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.zero,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Campaigns",
+                          style:
+                              TextStyle(fontFamily: "Montserrat", fontSize: 18),
+                          textAlign: TextAlign.start,
+                        ),
+                        Icon(Icons.keyboard_arrow_right, size: 36),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(24.0),
+                          child: Card(
+                            child: InkWell(
+                              onTap: () {},
+                              child: Column(
+                                children: [
+                                  Image.network(
+                                    "https://dummyimage.com/300x300/000/fff",
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Buy a coffee, another one is free!",
+                                          textAlign: TextAlign.start,
+                                        ),
+                                        SizedBox(height: 2),
+                                        Text(
+                                          "Starbucks",
+                                          textAlign: TextAlign.start,
+                                          style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            color: Colors.grey,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(24.0),
+                          child: Card(
+                            child: InkWell(
+                              onTap: () {},
+                              child: Column(
+                                children: [
+                                  Image.network(
+                                    "https://dummyimage.com/300x300/000/fff",
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Buy a coffee, another one is free!",
+                                          textAlign: TextAlign.start,
+                                        ),
+                                        SizedBox(height: 2),
+                                        Text(
+                                          "Starbucks",
+                                          textAlign: TextAlign.start,
+                                          style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            color: Colors.grey,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Center(
+                  child: FlatButton(
+                    textColor: Theme.of(context).accentColor,
+                    child: Text(
+                      "Check More Campaigns",
+                      style: TextStyle(fontFamily: "Montserrat"),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 24,
-          ),
-          CustomCarouselSlider(items: sliderItems),
         ],
       ),
     );
--- lib/screens/home/widgets/CarouselSliderWidget.dart
+++ lib/screens/home/widgets/CarouselSliderWidget.dart
@@ -11,24 +11,51 @@ class CustomCarouselSlider extends StatefulWidget {
 }
 
 class _CustomCarouselSliderState extends State<CustomCarouselSlider> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      items: widget.items,
-      options: CarouselOptions(
-        height: 200,
-        aspectRatio: 16 / 9,
-        viewportFraction: 0.8,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        reverse: false,
-        autoPlay: true,
-        autoPlayInterval: Duration(seconds: 10),
-        autoPlayAnimationDuration: Duration(milliseconds: 800),
-        autoPlayCurve: Curves.fastOutSlowIn,
-        enlargeCenterPage: true,
-        scrollDirection: Axis.horizontal,
-      ),
+    return Column(
+      children: [
+        CarouselSlider(
+          items: widget.items,
+          options: CarouselOptions(
+            height: 200,
+            aspectRatio: 16 / 9,
+            viewportFraction: 0.8,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 10),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (i, r) {
+              _currentIndex = i;
+              setState(() {});
+            },
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: widget.items.map((url) {
+            int index = widget.items.indexOf(url);
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentIndex == index
+                    ? Theme.of(context).accentColor
+                    : Theme.of(context).primaryColor,
+              ),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
